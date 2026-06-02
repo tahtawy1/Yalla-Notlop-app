83b573f1b7f1c9deb19fbd2fe0b5dd11c4052215
Taha Sabry <sabrytaha854@gmail.com>
2026-06-03 01:58:39 +0300

    Finish the splash/feature

---
+++ lib/core/constants/app_image_assets.dart
@@ -0,0 +1,7 @@
+class AppImageAssets {
+  static const String logo = 'assets/images/logo.png';
+  static const String burger = 'assets/images/burger.png';
+  static const String drink = 'assets/images/drink.png';
+  static const String feed = 'assets/images/feed.png';
+  static const String pizza = 'assets/images/pizza.png';
+}
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xffFF7A2F);
+
+  static const Color splashGradientStart = Color(0xffFF7A2F);
+  static const Color splashGradientEnd = Color(0xffFFEDE4);
+
+  static const Color splashTitleColor = Color(0xff3B2A1A);
+  static const Color splashSubtitleColor = Color(0xff4A3728);
+  static const Color splashCartBadge = Color(0xff3D7A3A);
+  static const Color splashIconCircle = Color(0x33FFFFFF);
+  static const Color splashDashedBorder = Color(0x55FF7A2F);
+  static const Color splashDivider = Color(0xffD4875A);
+  static const Color splashBottomText = Color(0xff5C3D2E);
+
+  static const Color splashIndicator = Color(0xff9C3F00);
+}
